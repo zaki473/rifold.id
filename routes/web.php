8a0e8ee7d7a748d08a0e041d6ac6b47961e3f32e
zaki <jakiathallah@gmail.com>
2025-11-12 14:51:14 +0700

    Resolve merge conflict in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('pages.home.home');
 })->name('home');
@@ -23,3 +22,38 @@ Route::get('/bestseller', function () {
     return view('pages.home.bestseller');
 })->name('bestseller');
 
+Route::get('/admin', function () {
+    return view('pages.admin.products');
+})->name('admin');
+
+Route::get('/admin/add_products', function () {
+    return view('pages.admin.add_products');
+})->name('add_products');
+
+Route::get('/add_images', function () {
+    return view('pages.admin.add_images');
+})->name('add_images');
+
+Route::get('/add_mixandmatch', function () {
+    return view('pages.admin.add_mixandmatch');
+})->name('add_mixandmatch');
+
+Route::get('/admin/add_video', function () {
+    return view('pages.admin.add_video');
+})->name('add_video');
+
+Route::get('/checkout', function () {
+    return view('pages.checkout.checkout');
+})->name('checkout');
+
+Route::post('/checkout/shipping', function () {
+    return view('pages.checkout.shipping');
+})->name('checkout.shipping');
+
+Route::post('/checkout/payment', function () {
+    return view('pages.checkout.payment');
+})->name('checkout.payment');
+
+Route::post('/checkout/confirmation', function () {
+    return view('pages.checkout.confirmation');
+})->name('checkout.confirmation');
