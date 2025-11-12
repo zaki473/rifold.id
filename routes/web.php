bf353a4d4776e785c2fef2b1c72e0e284db1851a
zaki <jakiathallah@gmail.com>
2025-11-12 14:48:40 +0700

    Fix routes/web.php after merge issues

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -24,42 +23,3 @@ Route::get('/bestseller', function () {
     return view('pages.home.bestseller');
 })->name('bestseller');
 
-Route::get('/admin', function () {
-    return view('pages.admin.products');
-})->name('admin');
-
-Route::get('/checkout', function () {
-    return view('pages.checkout.checkout');
-})->name('checkout');
-
-Route::get('/checkout', function () {
-    return view('pages.checkout.checkout');
-})->name('checkout.information');
-
-Route::post('/checkout/shipping', function () {
-    return view('pages.checkout.shipping');
-})->name('checkout.shipping');
-
-Route::post('/checkout/payment', function () {
-    return view('pages.checkout.payment');
-})->name('checkout.payment');
-
-Route::post('/checkout/confirmation', function () {
-    return view('pages.checkout.confirmation');
-})->name('checkout.confirmation');
-
-Route::get('/admin/add_products', function () {
-    return view('pages.admin.add_products');
-})->name('add_products');
-
-Route::get('/add_images', function () {
-    return view('pages.admin.add_images');
-})->name('add_images');
-
-Route::get('/add_mixandmatch', function () {
-    return view('pages.admin.add_mixandmatch');
-})->name('add_mixandmatch');
-
-Route::get('/admin/add_video', function () {
-    return view('pages.admin.add_video');
-})->name('add_video');
